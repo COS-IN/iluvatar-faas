9ff72c3fee544672baf591261833bd8bce2659a9
Alex Fuerst <fuersta.2013@gmail.com>
2022-07-19 13:26:50 -0400

    ansible basics done

--- src/Ilúvatar/load_balancer/src/main.rs
+++ src/Ilúvatar/load_balancer/src/main.rs
@@ -1,11 +1,14 @@
 use actix_web::{web::Data, App, HttpServer};
 use iluvatar_lib::transaction::{LOAD_BALANCER_TID, TransactionId};
+use iluvatar_lib::utils::config_utils::get_val;
 
 pub mod web_server;
 pub mod controller;
 pub mod logging;
 pub mod services;
+pub mod args;
 
+use crate::args::parse;
 use crate::controller::Controller;
 use crate::web_server::*;
 use crate::logging::make_logger;
@@ -16,8 +19,10 @@ use iluvatar_lib::load_balancer_api::config::Configuration;
 async fn main() -> std::io::Result<()> {
   iluvatar_lib::utils::file::ensure_temp_dir().unwrap();
   let tid: &TransactionId = &LOAD_BALANCER_TID;
+  let args = parse();
+  let config_pth = get_val("config", &args).unwrap();
 
-  let config = Configuration::boxed(&"".to_string()).unwrap();
+  let config = Configuration::boxed(&config_pth).unwrap();
   make_logger(config.logging.clone(), tid, flexi_logger::WriteMode::Direct);
 
   let server = Controller::new(config.clone());
