c6ea2c51931a1c4a55b92f914713c096d4b91fad
Alexander Fuerst <fuersta.2013@gmail.com>
2022-10-14 18:26:12 -0400

    cleanup

--- src/Ilúvatar/ilúvatar_library/src/graphite/mod.rs
+++ src/Ilúvatar/ilúvatar_library/src/graphite/mod.rs
@@ -19,12 +19,7 @@ pub struct GraphiteConfig {
 }
 
 // [{"target": "worker.load.loadavg", "tags": {"name": "worker.load.loadavg"}, "datapoints": [[3.81, 1658417660], [4.29, 1658417670]]}]
-// #[derive(Debug, serde::Deserialize)]
 type GraphiteResponse<T> = Vec<Metric<T>>;
-
-// struct GraphiteResponse<T> {
-//   pub data: Vec<Metric<T>>
-// }
 
 // {"target": "worker.load.loadavg", "tags": {"name": "worker.load.loadavg"}, "datapoints": [[3.81, 1658417660], [4.29, 1658417670]]}
 #[derive(Debug, serde::Deserialize)]
