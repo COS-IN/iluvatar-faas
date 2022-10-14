3d03bf56e8607dfabb998b8bd4dff14516d1fc8d
Alexander Fuerst <fuersta.2013@gmail.com>
2022-10-14 18:24:56 -0400

    simple plots of trace results

--- src/load/plotting/trace-latency-variance.py
+++ src/load/plotting/trace-latency-variance.py
@@ -14,7 +14,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 """
-Plot the worker overhead for each function in a trace, using violing plots and quantiles
+Plot the latency variance for each function in a trace
 """
 
 parser = argparse.ArgumentParser(description='')
@@ -25,8 +25,8 @@ args = parser.parse_args()
 
 bench_type = args.type.title()
 
-warm_overhead_ms_data = defaultdict(list)
-cold_overhead_ms_data = defaultdict(list)
+warm_ms_data = defaultdict(list)
+cold_ms_data = defaultdict(list)
 
 with open(args.file) as f:
   f.readline()
@@ -40,18 +40,13 @@ with open(args.file) as f:
       was_cold = False
     else:
       raise Exception(f"Illegal was_cold value: '{was_cold}'")
-    code_duration_ms = float(code_duration_sec) * 1000
-    overhead_ms = int(e2e_duration_ms) - code_duration_ms
     if was_cold:
-      cold_overhead_ms_data[function_name].append(overhead_ms)
+      cold_ms_data[function_name].append(int(e2e_duration_ms))
     else:
-      if overhead_ms > 100:
-        print(i, overhead_ms)
-        continue
-      warm_overhead_ms_data[function_name].append(overhead_ms)
+      warm_ms_data[function_name].append(int(e2e_duration_ms))
 
-cold_cnt = sum([len(x) for x in cold_overhead_ms_data.values()])
-warm_cnt = sum([len(x) for x in warm_overhead_ms_data.values()])
+cold_cnt = sum([len(x) for x in cold_ms_data.values()])
+warm_cnt = sum([len(x) for x in warm_ms_data.values()])
 
 ################################################################
 
@@ -61,38 +56,22 @@ fig.set_size_inches(5, 3)
 
 data = []
 labels = []
-quantlies = [0.5, 0.8, 0.9, 0.99, 0.999]
-for l, d in sorted(warm_overhead_ms_data.items(), key=lambda x: x[0]):
-  ax.plot(quantlies, np.quantile(d, quantlies), label=l)
+for l, d in sorted(warm_ms_data.items(), key=lambda x: x[0]):
+  print(np.std(d), np.var(d))
+  exit(0)
+  # ax.plot(quantlies, np.quantile(d, quantlies), label=l)
   data.append(d)
   labels.append(l)
 
-save_fname = os.path.join(args.outpath, "{}_warm_overhead_qs.png".format(args.type))
+save_fname = os.path.join(args.outpath, "{}_warm_variance.png".format(args.type))
 
-ax.set_title("{} Per-function Warm-Start Overhead Quantiles".format(bench_type))
+ax.set_title("{} Per-function Cold-Start Overhead Quantiles".format(bench_type))
 ax.set_ylabel("Platform overhead (ms)")
 ax.set_xlabel("Quantiles")
-# ax.legend()
+ax.legend()
 plt.savefig(save_fname, bbox_inches="tight")
 plt.close(fig)
 
-fig, ax = plt.subplots()
-plt.tight_layout()
-fig.set_size_inches(5, 3)
-
-positions = [i for i in range(len(labels))]
-
-ax.violinplot(data, positions=positions)
-save_fname = os.path.join(args.outpath, "{}_warm_overhead.png".format(args.type))
-
-ax.set_title("{} Per-function Warm-Start Overhead".format(bench_type))
-ax.set_ylabel("Platform overhead (ms)")
-ax.set_xlabel("Function")
-ax.set_xticks(positions)
-ax.set_xticklabels(labels, rotation = 90)
-
-plt.savefig(save_fname, bbox_inches="tight")
-plt.close(fig)
 
 ################################################################
 fig, ax = plt.subplots()
@@ -108,34 +87,16 @@ fig.set_size_inches(5, 3)
 data = []
 labels = []
 quantlies = [0.5, 0.8, 0.9, 0.99, 0.999]
-for l, d in sorted(cold_overhead_ms_data.items(), key=lambda x: x[0]):
+for l, d in sorted(cold_ms_data.items(), key=lambda x: x[0]):
   ax.plot(quantlies, np.quantile(d, quantlies), label=l)
   data.append(d)
   labels.append(l)
 
-save_fname = os.path.join(args.outpath, "{}_cold_overhead_qs.png".format(args.type))
+save_fname = os.path.join(args.outpath, "{}_cold_variance.png".format(args.type))
 
 ax.set_title("{} Per-function Cold-Start Overhead Quantiles".format(bench_type))
 ax.set_ylabel("Platform overhead (ms)")
 ax.set_xlabel("Quantiles")
 # ax.legend()
-plt.savefig(save_fname, bbox_inches="tight")
-plt.close(fig)
-
-fig, ax = plt.subplots()
-plt.tight_layout()
-fig.set_size_inches(5, 3)
-
-positions = [i for i in range(len(labels))]
-ax.violinplot(data, positions=positions)
-save_fname = os.path.join(args.outpath, "{}_cold_overhead.png".format(args.type))
-
-ax.set_title("{} Per-function Cold-Start Overhead".format(bench_type))
-ax.set_ylabel("Platform overhead (ms)")
-ax.set_xlabel("Function")
-
-ax.set_xticks(positions)
-ax.set_xticklabels(labels, rotation = 90)
-
 plt.savefig(save_fname, bbox_inches="tight")
 plt.close(fig)
