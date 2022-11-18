d1a09b6fa051d7b403b185f3841b2c0c1cee5bf7
Alexander Fuerst <fuersta.2013@gmail.com>
2022-11-18 12:33:37 -0500

    use better tokio type for waiting on invocation

--- src/Ilúvatar/ilúvatar_worker_library/src/services/invocation/invoker_structs.rs
+++ src/Ilúvatar/ilúvatar_worker_library/src/services/invocation/invoker_structs.rs
@@ -1,14 +1,16 @@
 use std::{sync::Arc, time::Duration};
-use iluvatar_library::{transaction::TransactionId, bail_error};
+use iluvatar_library::{transaction::TransactionId};
 use parking_lot::Mutex;
 use time::OffsetDateTime;
-use tokio::sync::Semaphore;
+use tokio::sync::Notify;
 use anyhow::Result;
 
 #[derive(Debug)]
 #[allow(unused)]
 pub struct InvocationResult {
+  /// The output from the invocation
   pub result_json: String,
+  /// The invocation time as tracked by the worker
   pub duration: Duration,
   pub attempts: u32,
   pub completed: bool
@@ -27,38 +29,45 @@ impl InvocationResult {
 pub type InvocationResultPtr = Arc<Mutex<InvocationResult>>;
 
 #[derive(Debug)]
-#[allow(unused)]
+/// A struct to hold a function while it is in the invocation queue
 pub struct EnqueuedInvocation {
+  pub fqdn: String,
+  /// Pointer where results will be stored on invocation completion
   pub result_ptr: InvocationResultPtr,
   pub function_name: String, 
   pub function_version: String, 
   pub json_args: String, 
   pub tid: TransactionId,
-  signal: Semaphore,
+  signal: Notify,
+  /// The local time at which the item was inserted into the queue
   pub queue_insert_time: OffsetDateTime,
 }
 
 impl EnqueuedInvocation {
-  pub fn new(function_name: String, function_version: String, json_args: String, tid: TransactionId, queue_insert_time: OffsetDateTime) -> Self {
+  pub fn new(fqdn: String, function_name: String, function_version: String, json_args: String, tid: TransactionId, queue_insert_time: OffsetDateTime) -> Self {
     EnqueuedInvocation {
+      fqdn,
       result_ptr: InvocationResult::boxed(),
       function_name,
       function_version,
       json_args,
       tid,
-      signal: Semaphore::new(0),
+      signal: Notify::new(),
       queue_insert_time
     }
   }
 
-  pub async fn wait(&self, tid: &TransactionId) -> Result<()> {
-    match self.signal.acquire().await {
-      Ok(_) => Ok(()),
-      Err(e) => bail_error!(tid=%tid, error=%e, "Failed to wait on enqueued invocation signal due to an error"),
-    }
+  /// Block the current thread until the invocation is complete
+  /// Only one waiter on an invocation is currently supported
+  /// If the invocation is complete, this will return immediately
+  pub async fn wait(&self, _tid: &TransactionId) -> Result<()> {
+    self.signal.notified().await;
+    Ok(())
   }
 
+  /// Signal the current waiter to wake up because the invocation result is available
+  /// If no one is waiting, the next person to check will continue immediately
   pub fn signal(&self) {
-    self.signal.add_permits(1);
+    self.signal.notify_one();
   }
 }
