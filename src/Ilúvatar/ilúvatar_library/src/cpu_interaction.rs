944f7217cb87d401d9e6352210a2d360bb068d10
Alexander Fuerst <fuersta.2013@gmail.com>
2023-01-03 12:23:44 -0500

    manually do cpu monitoring, stop using blocking external calls

--- src/Ilúvatar/ilúvatar_library/src/cpu_interaction.rs
+++ src/Ilúvatar/ilúvatar_library/src/cpu_interaction.rs
@@ -1,7 +1,7 @@
-use std::{path::{Path, PathBuf}, fs::File, io::Read, sync::Arc};
+use std::{path::{Path, PathBuf}, fs::File, io::{Read, BufRead}, sync::Arc};
 use tracing::{error, debug};
 use anyhow::Result;
-use crate::{transaction::TransactionId, nproc};
+use crate::{transaction::TransactionId, nproc, bail_error};
 
 const BASE_CPU_DIR: &str = "/sys/devices/system/cpu";
 
@@ -75,5 +75,129 @@ impl CPUService {
     }
 
     ret
+  }
+
+  pub fn instant_cpu_util(&self, tid: &TransactionId) -> Result<CPUUtilInstant> {
+    CPUUtilInstant::get(tid)
+  }
+  pub fn compute_cpu_util(&self, inst1: &CPUUtilInstant, inst2: &CPUUtilInstant) -> CPUUtilPcts {
+    if inst1.read_time > inst2.read_time {
+      inst1 - inst2
+    } else {
+      inst2 - inst1
+    }
+  }
+}
+
+/// The CPU usage metrics reported by /proc/stat
+pub struct CPUUtilInstant {
+  read_time: time::Instant,
+  cpu_user: u64,
+  cpu_nice: u64,
+  cpu_system: u64,
+  cpu_idle: u64,
+  cpu_iowait: u64,
+  cpu_irq: u64,
+  cpu_softirq: u64,
+  cpu_steal: u64,
+  cpu_guest: u64,
+  cpu_guest_nice: u64,
+}
+impl CPUUtilInstant {
+  pub fn get(tid: &TransactionId) -> Result<Self> {
+    let cpu_line = Self::read()?;
+    Self::parse(cpu_line, tid)
+  }
+  fn read() -> Result<String> {
+    let mut ret = String::new();
+    let mut b = match File::open("/proc/stat") {
+      Ok(f) => std::io::BufReader::new(f),
+      Err(e) => {return Err(e.into());},
+    };
+    loop {
+      match b.read_line(&mut ret)? {
+        0 => anyhow::bail!("Unable to find matching 'cpu ' line in /proc/stat"),
+        _ => {
+          if ret.starts_with("cpu ") {
+            return Ok(ret);
+          }
+        }
+      }
+    }
+  }
+  fn parse(mut line: String, tid: &TransactionId) -> Result<Self> {
+    if line.ends_with('\n') {
+      line.pop();
+    }
+    let strs: Vec<&str> = line.split(" ").filter(|str| str.len() > 0).collect();
+    Ok(Self {
+        read_time: time::Instant::now(),
+        cpu_user: Self::safe_get_val(&strs, 1, tid)?,
+        cpu_nice: Self::safe_get_val(&strs, 2, tid)?,
+        cpu_system: Self::safe_get_val(&strs,3, tid)?,
+        cpu_idle: Self::safe_get_val(&strs, 4, tid)?,
+        cpu_iowait: Self::safe_get_val(&strs, 5, tid)?,
+        cpu_irq: Self::safe_get_val(&strs, 6, tid)?,
+        cpu_softirq: Self::safe_get_val(&strs, 7, tid)?,
+        cpu_steal: Self::safe_get_val(&strs, 8, tid)?,
+        cpu_guest: Self::safe_get_val(&strs, 9, tid)?,
+        cpu_guest_nice: Self::safe_get_val(&strs, 10, tid)?,
+    })
+  }
+  fn safe_get_val(split_line: &Vec<&str>, pos: usize, tid: &TransactionId) -> Result<u64> {
+    if split_line.len() >= pos {
+      match split_line[pos].parse::<u64>() {
+        Ok(v) => Ok(v),
+        Err(e) => bail_error!(error=%e, tid=%tid, line=%split_line[pos], "Unable to parse string from /proc/stat"),
+      }
+    } else {
+      bail_error!(expected=pos, actual=split_line.len(), tid=%tid, "/proc/stat line was unexpectedly short!")
+    }
+  }
+}
+
+pub struct CPUUtilPcts {
+  pub read_diff: time::Duration,
+  pub cpu_user: f64,
+  pub cpu_nice: f64,
+  pub cpu_system: f64,
+  pub cpu_idle: f64,
+  pub cpu_iowait: f64,
+  pub cpu_irq: f64,
+  pub cpu_softirq: f64,
+  pub cpu_steal: f64,
+  pub cpu_guest: f64,
+  pub cpu_guest_nice: f64,
+}
+impl std::ops::Sub for &CPUUtilInstant {
+  type Output = CPUUtilPcts;
+
+  fn sub(self, rhs: Self) -> Self::Output {
+    let dur = self.read_time - rhs.read_time;
+    let user_diff = self.cpu_user - rhs.cpu_user;
+    let nice_diff = self.cpu_nice - rhs.cpu_nice;
+    let system_diff = self.cpu_system - rhs.cpu_system;
+    let idle_diff = self.cpu_idle - rhs.cpu_idle;
+    let iowait_diff = self.cpu_iowait - rhs.cpu_iowait;
+    let irq_diff = self.cpu_irq - rhs.cpu_irq;
+    let softirq_diff = self.cpu_softirq - rhs.cpu_softirq;
+    let steal_diff = self.cpu_steal - rhs.cpu_steal;
+    let guest_diff = self.cpu_guest - rhs.cpu_guest;
+    let guest_nice_diff = self.cpu_guest_nice - rhs.cpu_guest_nice;
+    let tot = (user_diff + nice_diff + system_diff + idle_diff + iowait_diff + irq_diff + 
+                    softirq_diff + steal_diff + guest_diff + guest_nice_diff) as f64;
+    CPUUtilPcts {
+      read_diff: dur,
+      cpu_user: (user_diff as f64 / tot)*100.0,
+      cpu_nice: (nice_diff as f64 / tot)*100.0,
+      cpu_system: (system_diff as f64 / tot)*100.0,
+      cpu_idle: (idle_diff as f64 / tot)*100.0,
+      cpu_iowait: (iowait_diff as f64 / tot)*100.0,
+      cpu_irq: (irq_diff as f64 / tot)*100.0,
+      cpu_softirq: (softirq_diff as f64 / tot)*100.0,
+      cpu_steal: (steal_diff as f64 / tot)*100.0,
+      cpu_guest: (guest_diff as f64 / tot)*100.0,
+      cpu_guest_nice: (guest_nice_diff as f64 / tot)*100.0,
+    }
   }
 }
--- src/Ilúvatar/ilúvatar_worker_library/src/services/status/status_service.rs
+++ src/Ilúvatar/ilúvatar_worker_library/src/services/status/status_service.rs
@@ -1,9 +1,9 @@
 
 use std::sync::Arc;
 use std::thread::JoinHandle;
-use iluvatar_library::cpu_interaction::CPUService;
+use iluvatar_library::cpu_interaction::{CPUService, CPUUtilInstant};
 use iluvatar_library::{nproc, threading};
-use tracing::{info, debug, error, warn};
+use tracing::{info, debug, error};
 use parking_lot::Mutex;
 use crate::services::containers::containermanager::ContainerManager;
 use crate::services::invocation::invoker_trait::Invoker;
@@ -25,12 +25,14 @@ pub struct StatusService {
   metrics: Vec<&'static str>,
   cpu: Arc<CPUService>,
   config: Arc<StatusConfig>,
+  cpu_instant: Mutex<CPUUtilInstant>,
 }
 
 impl StatusService {
   pub async fn boxed(cm: Arc<ContainerManager>, invoke: Arc<dyn Invoker>, graphite_cfg: Arc<GraphiteConfig>, worker_name: String, tid: &TransactionId, config: Arc<StatusConfig>) -> Result<Arc<Self>> {
     let (handle, sender) = 
           threading::os_thread::<Self>(config.report_freq_ms, iluvatar_library::transaction::STATUS_WORKER_TID.clone(), Arc::new(StatusService::update_status));
+    let cpu_svc = CPUService::boxed(tid)?;
 
     let ret = Arc::new(StatusService { 
       container_manager: cm, 
@@ -56,81 +58,12 @@ impl StatusService {
       metrics: vec!["worker.load.loadavg", "worker.load.cpu", 
                     "worker.load.queue", "worker.load.mem_pct", 
                     "worker.load.used_mem"],
-      cpu: CPUService::boxed(tid)?,
-      config
+      cpu_instant: Mutex::new(cpu_svc.instant_cpu_util(tid)?),
+      cpu: cpu_svc,
+      config,
     });
     sender.send(ret.clone()).unwrap();
     Ok(ret)
-  }
-
-  /// returns system CPU usage as reported by mpstat
-  /// numbers are (user, system, idle, wait)
-  fn mpstat(&self, tid: &TransactionId) -> (f64,f64,f64,f64) {
-    match execute_cmd("/usr/bin/mpstat", &vec!["1", "1"], None, tid) {
-      Ok(out) => {
-        let stdout = String::from_utf8_lossy(&out.stdout);
-        // Output will look like this:
-        // Linux 5.4.0-132-generic (v-021) 	12/02/2022 	_x86_64_	(96 CPU)
-
-        // 02:15:49 PM  CPU    %usr   %nice    %sys %iowait    %irq   %soft  %steal  %guest  %gnice   %idle
-        // 02:15:50 PM  all    1.98    0.00    2.16    0.66    0.00    0.30    0.00    0.00    0.00   94.90
-        // Average:     all    1.98    0.00    2.16    0.66    0.00    0.30    0.00    0.00    0.00   94.90
-
-        let lines = stdout.split("\n").filter(|str| str.len() > 0).collect::<Vec<&str>>();
-        debug!(tid=%tid, output=?lines, "mpstat output");
-        if lines.len() != 4 {
-          warn!(tid=%tid, "Had an unexpected number of output lines from mpstat '{}'", lines.len());
-          return (-1.0,-1.0,-1.0,-1.0);
-        }
-        let name_parts: Vec<&str> = lines[1].split(" ").filter(|str| str.len() > 0).collect();
-        let data_parts: Vec<&str> = lines.last().unwrap_or_else(||&"").split(" ").filter(|str| str.len() > 0).collect();
-        debug!(tid=%tid, name_parts=?name_parts, data_parts=?data_parts, "mpstat parts");
-        if data_parts[0] != "Average:" {
-          warn!(tid=%tid, data_parts=?data_parts, "unexpected start to data_parts");
-          return (-1.0,-1.0,-1.0,-1.0);
-        }
-        if data_parts.len()+1 != name_parts.len() {
-          warn!(tid=%tid, "mpstat output lines were not of equal length name_parts: '{}' vs data_parts: '{}'", name_parts.len(), data_parts.len());
-          return (-1.0,-1.0,-1.0,-1.0);
-        }
-        let mut found_data: std::collections::HashMap<&str, f64> = std::collections::HashMap::new();
-        for (pos, name) in name_parts.iter().enumerate() {
-          if name.starts_with("%") {
-            found_data.insert(name, self.parse(data_parts[pos-1], tid));
-          }
-        }
-        let mut user = 0.0;
-        user += self.get_or_zero(tid, &found_data, "%usr");
-        user += self.get_or_zero(tid, &found_data, "%nice");
-
-        let mut sys = self.get_or_zero(tid, &found_data, "%sys");
-        sys += self.get_or_zero(tid, &found_data, "%irq");
-        sys += self.get_or_zero(tid, &found_data, "%soft");
-        sys += self.get_or_zero(tid, &found_data, "%steal");
-        sys += self.get_or_zero(tid, &found_data, "%guest");
-        sys += self.get_or_zero(tid, &found_data, "%gnice");
-
-        let wait = self.get_or_zero(tid, &found_data, "%iowait");
-        let idle = self.get_or_zero(tid, &found_data, "%idle");
-
-        debug!(tid=%tid, "mpstat {} {} {} {}", user, sys, wait, idle);
-        (user,sys,idle,wait)
-      },
-      Err(e) => {
-        error!(tid=%tid, "unable to call mpstat because {}", e);
-        (-1.0,-1.0,-1.0,-1.0)
-      },
-    }
-  }
-
-  fn get_or_zero(&self, tid: &TransactionId, data: &std::collections::HashMap<&str, f64>, key: &str) -> f64 {
-    match data.get(key) {
-      Some(v) => *v,
-      None => {
-        debug!(tid=%tid, key=%key, "Was unable to find key in mpstat result");
-        0.0
-      },
-    }
   }
 
   fn uptime(&self, tid: &TransactionId) -> f64 {
@@ -160,7 +93,14 @@ impl StatusService {
   fn update_status(&self, tid: &TransactionId) {
     let _free_cs = self.container_manager.free_cores();
 
-    let (us, sy, id, wa) = self.mpstat(tid);
+    let cpu_now = match self.cpu.instant_cpu_util(tid) {
+      Ok(i) => i,
+      Err(e) => {
+        error!(tid=%tid, error=%e, "Unable to get instant cpu utilization");
+        return;
+      },
+    };
+
     let minute_load_avg = self.uptime(tid);
     let nprocs = match nproc(tid, false) {
       Ok(n) => n,
@@ -169,6 +109,11 @@ impl StatusService {
         0
       },
     };
+
+    let mut cpu_instant_lck = self.cpu_instant.lock();
+    let computed_util = self.cpu.compute_cpu_util(&cpu_now, &(*cpu_instant_lck));
+    *cpu_instant_lck = cpu_now;
+
     let queue_len = self.invoker_service.queue_len() as i64;
     let used_mem = self.container_manager.used_memory();
     let total_mem = self.container_manager.total_memory();
@@ -181,10 +126,10 @@ impl StatusService {
       queue_len,
       used_mem,
       total_mem,
-      cpu_us: us,
-      cpu_sy: sy,
-      cpu_id: id,
-      cpu_wa: wa,
+      cpu_us: computed_util.cpu_user + computed_util.cpu_nice,
+      cpu_sy: computed_util.cpu_system + computed_util.cpu_irq + computed_util.cpu_softirq + computed_util.cpu_steal + computed_util.cpu_guest + computed_util.cpu_guest_nice,
+      cpu_id: computed_util.cpu_idle,
+      cpu_wa: computed_util.cpu_iowait,
       load_avg_1minute: minute_load_avg,
       num_system_cores: nprocs,
       num_running_funcs: running,
@@ -194,25 +139,13 @@ impl StatusService {
     });
     info!(tid=%tid, status=%new_status,"current load status");
 
-    let values = vec![(minute_load_avg / nprocs as f64), (us+sy) as f64,
+    let values = vec![(minute_load_avg / nprocs as f64), (new_status.cpu_us+new_status.cpu_sy) as f64,
                                     queue_len as f64, (used_mem as f64 / total_mem as f64), 
                                     used_mem as f64];
     self.graphite.publish_metrics(&self.metrics, values, tid, self.tags.as_str());
 
     let mut current_status = self.current_status.lock();
     *current_status = new_status;
-  }
-
-  /// parse the string to an i64, used for getting info from vmstat
-  fn parse<T: std::str::FromStr + std::default::Default>(&self, string: &str, tid: &TransactionId) -> T
-  where <T as std::str::FromStr>::Err: std::fmt::Display {
-    match string.parse::<T>() {
-      Ok(r) => r,
-      Err(e) => {
-        error!(tid=%tid, "error parsing {}: {}", string, e);
-        T::default()
-      },
-    }
   }
 
   /// Returns the status and load of the worker
