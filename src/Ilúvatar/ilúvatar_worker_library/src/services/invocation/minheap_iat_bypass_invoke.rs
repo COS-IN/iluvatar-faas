bfa3b5731d464605abbe7ec75d46a8aa9c245e62
Abdul Rehman <abdulrehman94494@gmail.com>
2023-01-23 13:24:38 -0500

    minheap_iat_bypass

---
+++ src/Ilúvatar/ilúvatar_worker_library/src/services/invocation/minheap_iat_bypass_invoke.rs
@@ -0,0 +1,153 @@
+use std::sync::atomic::AtomicU32;
+use std::sync::Arc;
+use crate::services::invocation::invoker_trait::create_concurrency_semaphore;
+use crate::worker_api::worker_config::{FunctionLimits, InvocationConfig};
+use crate::services::containers::containermanager::ContainerManager;
+use iluvatar_library::characteristics_map::compare_f64;
+use iluvatar_library::{transaction::{TransactionId, INVOKER_QUEUE_WORKER_TID}, threading::tokio_runtime, characteristics_map::CharacteristicsMap};
+use iluvatar_library::logging::LocalTime;
+use anyhow::Result;
+use parking_lot::Mutex;
+use tokio::sync::{Notify, Semaphore};
+use tracing::debug;
+use super::{invoker_trait::{Invoker, monitor_queue}, async_tracker::AsyncHelper, invoker_structs::EnqueuedInvocation};
+use std::collections::BinaryHeap;
+use std::cmp::Ordering;  
+
+#[derive(Debug)]
+pub struct MHQIATBPEnqueuedInvocation {
+  x: Arc<EnqueuedInvocation>,
+  iat: f64
+}
+
+impl MHQIATBPEnqueuedInvocation {
+  fn new( x: Arc<EnqueuedInvocation>, iat: f64 ) -> Self {
+    MHQIATBPEnqueuedInvocation {
+      x,
+      iat
+    }
+  }
+}
+
+impl Eq for MHQIATBPEnqueuedInvocation {
+}
+
+impl Ord for MHQIATBPEnqueuedInvocation {
+  fn cmp(&self, other: &Self) -> Ordering {
+    compare_f64( &self.iat, &other.iat ).reverse()
+  }
+}
+
+impl PartialOrd for MHQIATBPEnqueuedInvocation {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(compare_f64( &self.iat, &other.iat ).reverse())
+  }
+}
+
+impl PartialEq for MHQIATBPEnqueuedInvocation {
+  fn eq(&self, other: &Self) -> bool {
+    self.iat == other.iat
+  }
+}
+
+pub struct MinHeapIATBPInvoker {
+  pub cont_manager: Arc<ContainerManager>,
+  pub async_functions: AsyncHelper,
+  pub function_config: Arc<FunctionLimits>,
+  pub invocation_config: Arc<InvocationConfig>,
+  pub invoke_queue: Arc<Mutex<BinaryHeap<Arc<MHQIATBPEnqueuedInvocation>>>>,
+  pub cmap: Arc<CharacteristicsMap>,
+  _worker_thread: std::thread::JoinHandle<()>,
+  queue_signal: Notify,
+  clock: LocalTime,
+  concurrency_semaphore: Arc<Semaphore>,
+  bypass_running: AtomicU32
+}
+
+impl MinHeapIATBPInvoker {
+  pub fn new(cont_manager: Arc<ContainerManager>, function_config: Arc<FunctionLimits>, invocation_config: Arc<InvocationConfig>, tid: &TransactionId, cmap: Arc<CharacteristicsMap>) -> Result<Arc<Self>> {
+    let (handle, tx) = tokio_runtime(invocation_config.queue_sleep_ms, INVOKER_QUEUE_WORKER_TID.clone(), monitor_queue, Some(MinHeapIATBPInvoker::wait_on_queue), Some(function_config.cpu_max as usize))?;
+    if invocation_config.bypass_duration_ms <= 0 {
+      anyhow::bail!("Cannot have a bypass_duration_ms of zero for this invoker queue policy")
+    }
+    let svc = Arc::new(MinHeapIATBPInvoker {
+      concurrency_semaphore: create_concurrency_semaphore(invocation_config.concurrent_invokes)?,
+      cont_manager,
+      function_config,
+      invocation_config,
+      async_functions: AsyncHelper::new(),
+      queue_signal: Notify::new(),
+      invoke_queue: Arc::new(Mutex::new(BinaryHeap::new())),
+      cmap,
+      _worker_thread: handle,
+      clock: LocalTime::new(tid)?,
+      bypass_running: AtomicU32::new(0)
+    });
+    tx.send(svc.clone())?;
+    debug!(tid=%tid, "Created MinHeapIATBPInvoker");
+    Ok(svc)
+  }
+
+  /// Wait on the Notify object for the queue to be available again
+  async fn wait_on_queue(invoker_svc: Arc<MinHeapIATBPInvoker>, tid: TransactionId) {
+    invoker_svc.queue_signal.notified().await;
+    debug!(tid=%tid, "Invoker waken up by signal");
+  }
+}
+
+#[tonic::async_trait]
+impl Invoker for MinHeapIATBPInvoker {
+  fn peek_queue(&self) -> Option<Arc<EnqueuedInvocation>> {
+    let r = self.invoke_queue.lock();
+    let r = r.peek()?;
+    let r = r.x.clone();
+    return Some(r);
+  }
+  fn pop_queue(&self) -> Arc<EnqueuedInvocation> {
+    let mut invoke_queue = self.invoke_queue.lock();
+    let v = invoke_queue.pop().unwrap();
+    let v = v.x.clone();
+    let top = invoke_queue.peek();
+    let func_name; 
+    match top {
+        Some(e) => func_name = e.x.function_name.clone(),
+        None => func_name = "empty".to_string()
+    }
+    debug!(tid=%v.tid,  component="minheap", "Popped item from queue minheap - len: {} popped: {} top: {} ",
+           invoke_queue.len(),
+           v.function_name,
+           func_name );
+    v
+  }
+
+  fn cont_manager(&self) -> &Arc<ContainerManager>  { &self.cont_manager }
+  fn function_config(&self) -> &Arc<FunctionLimits>  { &self.function_config }
+  fn invocation_config(&self) -> &Arc<InvocationConfig>  { &self.invocation_config }
+  fn queue_len(&self) -> usize {
+    self.invoke_queue.lock().len()
+  }
+  fn timer(&self) -> &LocalTime {
+    &self.clock
+  }
+  fn async_functions<'a>(&'a self) -> &'a AsyncHelper {
+    &self.async_functions
+  }
+  fn concurrency_semaphore(&self) -> Option<Arc<Semaphore>> {
+    Some(self.concurrency_semaphore.clone())
+  }
+  fn char_map(&self) -> &Arc<CharacteristicsMap> {
+    &self.cmap
+  }
+  fn bypass_running(&self) -> Option<&AtomicU32> { Some(&self.bypass_running) }
+  #[cfg_attr(feature = "full_spans", tracing::instrument(skip(self, item, _index), fields(tid=%item.tid)))]
+  fn add_item_to_queue(&self, item: &Arc<EnqueuedInvocation>, _index: Option<usize>) {
+    let mut queue = self.invoke_queue.lock();
+    let iat = self.cmap.get_iat( &item.fqdn );
+    queue.push(MHQIATBPEnqueuedInvocation::new(item.clone(), iat ).into());
+    debug!(tid=%item.tid,  component="minheap", "Added item to front of queue minheap - len: {} arrived: {} top: {} ", 
+                        queue.len(),
+                        item.function_name,
+                        queue.peek().unwrap().x.function_name );
+    self.queue_signal.notify_waiters();
+  }
+}
