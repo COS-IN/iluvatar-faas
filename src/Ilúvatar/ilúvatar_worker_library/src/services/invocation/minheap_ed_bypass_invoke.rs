0906e9db6d62f79790866d352be2dbde1be64138
Abdul Rehman <abdulrehman94494@gmail.com>
2023-01-23 13:16:50 -0500

    minheap_ed_bypass

--- src/Ilúvatar/ilúvatar_worker_library/src/services/invocation/minheap_ed_bypass_invoke.rs
+++ src/Ilúvatar/ilúvatar_worker_library/src/services/invocation/minheap_ed_bypass_invoke.rs
@@ -1,5 +1,6 @@
 use std::sync::atomic::AtomicU32;
 use std::sync::Arc;
+use std::time::{SystemTime, UNIX_EPOCH};
 use crate::services::invocation::invoker_trait::create_concurrency_semaphore;
 use crate::worker_api::worker_config::{FunctionLimits, InvocationConfig};
 use crate::services::containers::containermanager::ContainerManager;
@@ -9,51 +10,60 @@ use iluvatar_library::logging::LocalTime;
 use anyhow::Result;
 use parking_lot::Mutex;
 use tokio::sync::{Notify, Semaphore};
-use tracing::{debug};
+use tracing::debug;
 use super::{invoker_trait::{Invoker, monitor_queue}, async_tracker::AsyncHelper, invoker_structs::EnqueuedInvocation};
 use std::collections::BinaryHeap;
-use std::cmp::Ordering; 
+use std::cmp::Ordering;
 
-#[derive(Debug)]
-pub struct MHQBPEnqueuedInvocation {
-    x: Arc<EnqueuedInvocation>,
-    exectime: f64
+fn time_since_epoch() -> f64 {
+    let start = SystemTime::now();
+    start.duration_since( UNIX_EPOCH )
+         .expect("Time went backwards")
+         .as_secs_f64()
 }
 
-impl MHQBPEnqueuedInvocation {
-    fn new( x: Arc<EnqueuedInvocation>, exectime: f64 ) -> Self {
-        MHQBPEnqueuedInvocation {
+#[derive(Debug)]
+pub struct MHQEDBPEnqueuedInvocation {
+    x: Arc<EnqueuedInvocation>,
+    deadline: f64
+}
+
+impl MHQEDBPEnqueuedInvocation {
+    fn new( x: Arc<EnqueuedInvocation>, deadline: f64 ) -> Self {
+        MHQEDBPEnqueuedInvocation {
             x,
-            exectime
+            deadline
         }
     }
 }
-impl Eq for MHQBPEnqueuedInvocation {
+
+impl Eq for MHQEDBPEnqueuedInvocation {
 }
-impl Ord for MHQBPEnqueuedInvocation {
+
+impl Ord for MHQEDBPEnqueuedInvocation {
  fn cmp(&self, other: &Self) -> Ordering {
-    compare_f64( &self.exectime, &other.exectime ).reverse()
+    compare_f64( &self.deadline, &other.deadline ).reverse()
  }
 }
 
-impl PartialOrd for MHQBPEnqueuedInvocation {
+impl PartialOrd for MHQEDBPEnqueuedInvocation {
  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-     Some(compare_f64( &self.exectime, &other.exectime ).reverse())
- }
+    Some(compare_f64( &self.deadline, &other.deadline ).reverse())
+  }
 }
 
-impl PartialEq for MHQBPEnqueuedInvocation {
+impl PartialEq for MHQEDBPEnqueuedInvocation {
  fn eq(&self, other: &Self) -> bool {
-     self.exectime == other.exectime
+     self.deadline == other.deadline
  }
 }
 
-pub struct MinHeapBPInvoker {
+pub struct MinHeapEDBPInvoker {
   pub cont_manager: Arc<ContainerManager>,
   pub async_functions: AsyncHelper,
   pub function_config: Arc<FunctionLimits>,
   pub invocation_config: Arc<InvocationConfig>,
-  pub invoke_queue: Arc<Mutex<BinaryHeap<Arc<MHQBPEnqueuedInvocation>>>>,
+  pub invoke_queue: Arc<Mutex<BinaryHeap<Arc<MHQEDBPEnqueuedInvocation>>>>,
   pub cmap: Arc<CharacteristicsMap>,
   _worker_thread: std::thread::JoinHandle<()>,
   queue_signal: Notify,
@@ -62,13 +72,13 @@ pub struct MinHeapBPInvoker {
   bypass_running: AtomicU32
 }
 
-impl MinHeapBPInvoker {
+impl MinHeapEDBPInvoker {
   pub fn new(cont_manager: Arc<ContainerManager>, function_config: Arc<FunctionLimits>, invocation_config: Arc<InvocationConfig>, tid: &TransactionId, cmap: Arc<CharacteristicsMap>) -> Result<Arc<Self>> {
-    let (handle, tx) = tokio_runtime(invocation_config.queue_sleep_ms, INVOKER_QUEUE_WORKER_TID.clone(), monitor_queue, Some(MinHeapBPInvoker::wait_on_queue), Some(function_config.cpu_max as usize))?;
+    let (handle, tx) = tokio_runtime(invocation_config.queue_sleep_ms, INVOKER_QUEUE_WORKER_TID.clone(), monitor_queue, Some(MinHeapEDBPInvoker::wait_on_queue), Some(function_config.cpu_max as usize))?;
     if invocation_config.bypass_duration_ms <= 0 {
       anyhow::bail!("Cannot have a bypass_duration_ms of zero for this invoker queue policy")
     }
-    let svc = Arc::new(MinHeapBPInvoker {
+    let svc = Arc::new(MinHeapEDBPInvoker {
       concurrency_semaphore: create_concurrency_semaphore(invocation_config.concurrent_invokes)?,
       cont_manager,
       function_config,
@@ -82,34 +92,31 @@ impl MinHeapBPInvoker {
       bypass_running: AtomicU32::new(0)
     });
     tx.send(svc.clone())?;
-    debug!(tid=%tid, "Created MinHeapBPInvoker");
+    debug!(tid=%tid, "Created MinHeapEDBPInvoker");
     Ok(svc)
   }
 
   /// Wait on the Notify object for the queue to be available again
-  async fn wait_on_queue(invoker_svc: Arc<MinHeapBPInvoker>, tid: TransactionId) {
+  async fn wait_on_queue(invoker_svc: Arc<MinHeapEDBPInvoker>, tid: TransactionId) {
     invoker_svc.queue_signal.notified().await;
     debug!(tid=%tid, "Invoker waken up by signal");
   }
 }
 
 #[tonic::async_trait]
-impl Invoker for MinHeapBPInvoker {
+impl Invoker for MinHeapEDBPInvoker {
   fn peek_queue(&self) -> Option<Arc<EnqueuedInvocation>> {
     let r = self.invoke_queue.lock();
     let r = r.peek()?;
-    let r = r.x.clone();
-    return Some(r);
+    Some(r.x.clone())
   }
   fn pop_queue(&self) -> Arc<EnqueuedInvocation> {
     let mut invoke_queue = self.invoke_queue.lock();
     let v = invoke_queue.pop().unwrap();
     let v = v.x.clone();
-    let top = invoke_queue.peek();
-    let func_name; 
-    match top {
-        Some(e) => func_name = e.x.function_name.clone(),
-        None => func_name = "empty".to_string()
+    let mut func_name = "empty"; 
+    if let Some(e) = invoke_queue.peek() {
+      func_name = e.x.function_name.as_str();
     }
     debug!(tid=%v.tid,  component="minheap", "Popped item from queue minheap - len: {} popped: {} top: {} ",
            invoke_queue.len(),
@@ -140,7 +147,8 @@ impl Invoker for MinHeapBPInvoker {
   #[cfg_attr(feature = "full_spans", tracing::instrument(skip(self, item, _index), fields(tid=%item.tid)))]
   fn add_item_to_queue(&self, item: &Arc<EnqueuedInvocation>, _index: Option<usize>) {
     let mut queue = self.invoke_queue.lock();
-    queue.push(MHQBPEnqueuedInvocation::new(item.clone(), self.cmap.get_exec_time(&item.fqdn)).into());
+    let deadline = self.cmap.get_exec_time(&item.fqdn) + time_since_epoch();
+    queue.push(MHQEDBPEnqueuedInvocation::new(item.clone(), deadline ).into());
     debug!(tid=%item.tid,  component="minheap", "Added item to front of queue minheap - len: {} arrived: {} top: {} ", 
                         queue.len(),
                         item.function_name,
