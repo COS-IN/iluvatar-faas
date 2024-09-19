1768d3511f9f1b38205a5092ebba6748cc88dc73
Abdul Rehman <abdulrehman94494@gmail.com>
2024-09-19 15:18:39 -0400

    func_characs -> func_metadata in all the other places.

--- src/Ilúvatar/iluvatar_bpf_library/src/bpf/charmap.bpf.c
+++ src/Ilúvatar/iluvatar_bpf_library/src/bpf/charmap.bpf.c
@@ -3,7 +3,7 @@
 #include <bpf/bpf_endian.h>
 
 #define MAX_FUNCS 50 
-#define KEYSIZE 15 // because the kernel fs inode name is 15 characters 
+#define FUNC_METADATA_KEYSIZE 15 // because the kernel fs inode name is 15 characters 
 
 typedef struct CharVal{
     u32 prio;
@@ -16,9 +16,9 @@ typedef struct CharVal{
 struct {
   __uint(type, BPF_MAP_TYPE_HASH);
   __uint(max_entries, MAX_FUNCS);
-  __uint(key_size, sizeof(char)*KEYSIZE);         /* cgrp ID */
+  __uint(key_size, sizeof(char)*FUNC_METADATA_KEYSIZE);         /* cgrp ID */
   __uint(value_size, sizeof(CharVal_t)); /* Value Structure */
-} func_characs SEC(".maps");
+} func_metadata SEC(".maps");
 
 char _license[] SEC("license") = "GPL";
 
